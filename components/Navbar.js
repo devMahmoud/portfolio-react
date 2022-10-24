827764c44eabcd1464feb056b43a0e1b842ada5c
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-10-24 12:18:40 +0200

    Update url for analytics

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = ({darkMode, setDarkMode}) => (
                 <a
                   id="resume-link"
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="https://drive.google.com/file/d/1B1CQoTCArkX2MEJFmYvHDuC-Hvv2yfrL/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1y3qX-tVDIzAEVkwXjFBX3dvWzutFJR72/view?usp=sharing"
                   target="blank"
                 >
                   Resume
